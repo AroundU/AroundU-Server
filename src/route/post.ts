3ce7e1478ad121422ce07230f5f5a8a9b548ef3e
JulienDuf <dufresne_julien@hotmail.ca>
2017-03-05 10:29:59 -0500

    Fix message

--- src/route/post.ts
+++ src/route/post.ts
@@ -50,8 +50,8 @@ module Route {
                             mimetype: req.file.mimetype,
                             name: req.file.filename,
                             size: req.file.size,
-                            url: "images/" + url + "image-" + req.file.filename + ".png",
-                            thumbnail: "images/" + url + "resized-" + req.file.filename + ".png"
+                            url: url + "images/image-" + req.file.filename + ".png",
+                            thumbnail: url + "images/resized-" + req.file.filename + ".png"
                         });
                     } catch (err) {
                         res.status(HttpStatus.Internal_Server_Error).json({ success: false, err: err, msg: null });
